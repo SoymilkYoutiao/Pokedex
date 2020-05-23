23674aa5c25e00fe0519f73438d7139830748ec2
Eric Chen <eric770506@gmail.com>
2020-05-23 23:48:42 +0800

    Updates

---
+++ precache-manifest.35154c45f7e8d44fcef612ac4ddc832e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3463e280e1c97732ffb7c5634efb9e9",
+    "url": "/Pokedex/index.html"
+  },
+  {
+    "revision": "44297857fe38440ac353",
+    "url": "/Pokedex/static/css/main.e0181a7c.chunk.css"
+  },
+  {
+    "revision": "d1f9dbed404dde30e5df",
+    "url": "/Pokedex/static/js/2.12e1356f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Pokedex/static/js/2.12e1356f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44297857fe38440ac353",
+    "url": "/Pokedex/static/js/main.e0f78a4e.chunk.js"
+  },
+  {
+    "revision": "e166d3a3ca0c2569d4b7",
+    "url": "/Pokedex/static/js/runtime-main.eb7fafaf.js"
+  }
+]);
